476ca492a93fbab684653731f4715c7be4466ec2
Sofien Ben Ktib <63020241+SofienBenKtib@users.noreply.github.com>
2023-11-16 00:05:04 +0100

    adding todo component

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,26 @@ import React, { useState } from "react";
 import "./App.css";
 import InputField from "./components/InputField";
 import { Todo } from "./model/model";
+import TodoList from "./components/TodoList";
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
   const [todos, setTodos] = useState<Todo[]>([]);
-  //32:40
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo: todo, isDone: false }]);
+      setTodo("");
+    }
+  };
+  console.log(todos);
   return (
     <div className="App">
       <span className="heading">S-TODO</span>
-      <InputField todo={todo} setTodo={setTodo} />
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      {/** TODO LIST */}
+      <TodoList todos={todos} setTodos={setTodos} />
     </div>
   );
 };
---
+++ src/components/SingleTodo.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+//44:45
+const SingleTodo = () => {
+  return <div>SingleTodo</div>;
+};
+
+export default SingleTodo;
